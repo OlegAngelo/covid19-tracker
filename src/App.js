3b5bac7a311f6ff609e24b73f7f695f68fb6d10a
OlegAngelo <angelonino1999@gmail.com>
2021-12-11 17:30:58 +0800

    added dropdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React from 'react';
+
+import { 
+  FormControl, 
+  Select, 
+  MenuItem
+} from '@mui/material';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-       TEST12345
-       123454y54u4yrtwerfds
-      </h1>
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown" >
+        <Select 
+          variant="outlined"
+          value="dropdown"
+        >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+          <MenuItem value="worldwide">Option 4</MenuItem>
+        </Select>
+      </FormControl>
       
     </div>
   );
